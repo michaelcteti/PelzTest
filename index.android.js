6cd83387fc145dd7a90a0a4778eeba6b39fb11cf
Michael Teti <michael.teti@arcadiasolutions.com>
2017-01-26 14:55:52 -0500

    Short game test summary

--- index.android.js
+++ index.android.js
@@ -7,10 +7,13 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Slider,
   StyleSheet,
   Text,
   TextInput,
-  View
+  View,
+  ScrollView,
+  Button
 } from 'react-native';
 
 class PelzTest extends Component {
@@ -20,9 +23,7 @@ class PelzTest extends Component {
         <Text style={styles.welcome}>
           Dave Pelz Tests/Handicap
         </Text>
-        <ShortGameTestSummary />
         <ShortGameTestInput />
-        <PuttingTestSummary />
       </View>
     );
   }
@@ -67,28 +68,118 @@ class PuttingTestSummary extends Component {
   }
 }
 
+class ScoreSlider extends Component {
+  render() {
+    return (
+      <Slider
+        minimumValue={0}
+        maximumValue={this.props.max}
+        onValueChange={this.props.valueFunction}
+        step={1}
+        style={{height: 20, margin: 10, width: 300}}
+      />
+    )
+  }
+}
+
 class ShortGameTestInput extends Component {
   constructor(props) {
     super(props);
-    this.state = {threeFourthWedgeScore: '0'};
+    this.state = {
+      threeFourthWedgeScore: 0,
+      halfWedgeScore: 0,
+      longSandScore: 0,
+      shortSandScore: 0,
+      longChipScore: 0,
+      shortChipScore: 0,
+      pitchFairwayScore: 0,
+      pitchRoughScore: 0,
+      cutLobScore: 0,
+      totalScore: 0
+    };
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollcontainer} >
         <Text style={styles.welcome}>
           Short Game Test Inputs
         </Text>
-        <Text>3/4 Wedge</Text>
-        <TextInput
-          enum='numeric'
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          placeholder="0"
-          onChangeText={(threeFourthWedgeScore) => this.setState({threeFourthWedgeScore})}
-          value={this.state.threeFourthWedgeScore}
+        <Text style={styles.welcome}>
+          Total Score: {this.state.totalScore}
+        </Text>
+
+        <Button
+          onPress={() => this.setState({totalScore: this.state.threeFourthWedgeScore +
+            this.state.halfWedgeScore + this.state.longSandScore + this.state.shortSandScore +
+            this.state.longChipScore + this.state.shortChipScore + this.state.pitchFairwayScore +
+            this.state.pitchRoughScore + this.state.cutLobScore})}
+          title="Add Scores"
         />
+
+        <Text>3/4 Wedge</Text>
         <Text>{this.state.threeFourthWedgeScore}</Text>
-      </View>
+        <ScoreSlider
+          max={17}
+          valueFunction={(value) => this.setState({threeFourthWedgeScore: value})}
+        />
+
+        <Text>1/2 Wedge</Text>
+        <Text>{this.state.halfWedgeScore}</Text>
+        <ScoreSlider
+          max={16}
+          valueFunction={(value) => this.setState({halfWedgeScore: value})}
+        />
+
+        <Text>Long Sand</Text>
+        <Text>{this.state.longSandScore}</Text>
+        <ScoreSlider
+          max={12}
+          valueFunction={(value) => this.setState({longSandScore: value})}
+        />
+
+        <Text>Short Sand</Text>
+        <Text>{this.state.shortSandScore}</Text>
+        <ScoreSlider
+          max={16}
+          valueFunction={(value) => this.setState({shortSandScore: value})}
+        />
+
+        <Text>Long Chip</Text>
+        <Text>{this.state.longChipScore}</Text>
+        <ScoreSlider
+          max={20}
+          valueFunction={(value) => this.setState({longChipScore: value})}
+        />
+
+        <Text>Short Chip</Text>
+        <Text>{this.state.shortChipScore}</Text>
+        <ScoreSlider
+          max={24}
+          valueFunction={(value) => this.setState({shortChipScore: value})}
+        />
+
+        <Text>Pitch Fairway</Text>
+        <Text>{this.state.pitchFairwayScore}</Text>
+        <ScoreSlider
+          max={19}
+          valueFunction={(value) => this.setState({pitchFairwayScore: value})}
+        />
+
+        <Text>Pitch Rough</Text>
+        <Text>{this.state.pitchRoughScore}</Text>
+        <ScoreSlider
+          max={17}
+          valueFunction={(value) => this.setState({pitchRoughScore: value})}
+        />
+
+        <Text>Cut Lob</Text>
+        <Text>{this.state.cutLobScore}</Text>
+        <ScoreSlider
+          max={14}
+          valueFunction={(value) => this.setState({cutLobScore: value})}
+        />
+      </ScrollView>
     )
   }
 }
@@ -126,7 +217,11 @@ export class PuttingHandicap extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  scrollcontainer: {
+    width: 400,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
